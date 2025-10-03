66b4d84736416a8e20224edf30f63edec54d1781
aguzpos332 <233921768+AidanGuzmanPostigo@users.noreply.github.com>
2025-10-03 18:00:14 +0200

    Add files via upload

---
+++ PrimerPrograma/src/tema2_eclipse/HelloEclipse.java
@@ -0,0 +1,22 @@
+package tema2_eclipse;
+public class HelloEclipse {
+	public static void main(String[] args) {
+		//Este programa imprime mensajes en consola
+		System.out.println("¡Mi primer programa");
+		System.out.println("Reglas básicas de Java:");
+		System.out.println("1) Distingue MAYÚSCULAS y minúsculas");
+		System.out.println("2) Cada instrucción termina con ;");
+		/*
+		 * Autor: Aidan Guzmán Postigo Fecha: 03/10/2025
+		 */
+		int x = 3;
+		System.out.print("Este código no está");
+		System.out.print(" muy bien ");
+		System.out.print("tabulado");
+		x = 7;
+		System.out.println(x);
+		int contadorMensajes = 0;
+		contadorMensajes++;
+		System.out.println("Mensajes impresos: " + contadorMensajes);
+	}
+}
